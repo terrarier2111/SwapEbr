f9f1df9ff1b10704a62e75103e5d9ceffc6ed700
threadexception <hannes.gaumann@outlook.de>
2024-03-25 16:41:01 +0100

    Add aarc to benchmark

--- benches/swap_ebr.rs
+++ benches/swap_ebr.rs
@@ -1,5 +1,7 @@
 extern crate criterion;
 
+use aarc::Arc as AArc;
+use aarc::AtomicArc;
 use arc_swap::ArcSwap;
 use criterion::Criterion;
 use rand::random;
@@ -130,6 +132,49 @@ fn main() {
                         }
                         for _ in 0..20000 {
                             tmp.store(Arc::new(random()));
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
+    c.bench_function("aarc_single_single", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..1 {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            let l1 = tmp.load(Ordering::Acquire);
+                            black_box(l1);
+                        }
+                    }));
+                }
+                for _ in 0..1 {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            tmp.store(Arc::new(random()), Ordering::Release);
                         }
                     }));
                 }
@@ -299,6 +344,57 @@ fn main() {
         });
     });
 
+    c.bench_function("aarc_single_multi", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..1 {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            let l1 = black_box(tmp.load(Ordering::Acquire));
+                            let l2 = black_box(tmp.load(Ordering::Acquire));
+                            let l3 = black_box(tmp.load(Ordering::Acquire));
+                            let l4 = black_box(tmp.load(Ordering::Acquire));
+                            let l5 = black_box(tmp.load(Ordering::Acquire));
+                            drop(black_box(l1));
+                            drop(black_box(l2));
+                            drop(black_box(l3));
+                            drop(black_box(l4));
+                            drop(black_box(l5));
+                        }
+                    }));
+                }
+                for _ in 0..1 {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            tmp.store(Arc::new(rand::random()), Ordering::Release);
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
     // ---
 
     c.bench_function("multi_single", |b| {
@@ -446,6 +542,59 @@ fn main() {
                         /*200*/
                         {
                             tmp.store(Arc::new(rand::random()));
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
+    c.bench_function("aarc_multi_single", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..20
+                /*5*//*1*/
+                {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000
+                        /*200*/
+                        {
+                            let l1 = tmp.load(Ordering::Acquire);
+                            black_box(l1);
+                        }
+                    }));
+                }
+                for _ in 0..20
+                /*5*//*1*/
+                {
+                    // let send = send.clone();
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        // let send = send.clone();
+                        for _ in 0..20000
+                        /*200*/
+                        {
+                            tmp.store(Arc::new(rand::random()), Ordering::Release);
                         }
                     }));
                 }
@@ -645,6 +794,67 @@ fn main() {
         });
     });
 
+    c.bench_function("aarc_multi_multi", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..20
+                /*5*//*1*/
+                {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000
+                        /*200*/
+                        {
+                            let l1 = tmp.load(Ordering::Acquire);
+                            let l2 = tmp.load(Ordering::Acquire);
+                            let l3 = tmp.load(Ordering::Acquire);
+                            let l4 = tmp.load(Ordering::Acquire);
+                            let l5 = tmp.load(Ordering::Acquire);
+                            black_box(l1);
+                            black_box(l2);
+                            black_box(l3);
+                            black_box(l4);
+                            black_box(l5);
+                        }
+                    }));
+                }
+                for _ in 0..20
+                /*5*//*1*/
+                {
+                    // let send = send.clone();
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        // let send = send.clone();
+                        for _ in 0..20000
+                        /*200*/
+                        {
+                            tmp.store(Arc::new(rand::random()), Ordering::Release);
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
     // ---
 
     c.bench_function("read_heavy_single", |b| {
@@ -731,6 +941,39 @@ fn main() {
                         }
                         for _ in 0..20000 {
                             let l1 = tmp.load();
+                            black_box(l1);
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
+    c.bench_function("aarc_read_heavy_single", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..20
+                /*5*//*1*/
+                {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            let l1 = tmp.load(Ordering::Acquire);
                             black_box(l1);
                         }
                     }));
@@ -841,6 +1084,37 @@ fn main() {
         });
     });
 
+    c.bench_function("aarc_read_light_single", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..1 {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            let l1 = tmp.load(Ordering::Acquire);
+                            black_box(l1);
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
     // ---
 
     c.bench_function("read_light_multi", |b| {
@@ -921,6 +1195,37 @@ fn main() {
                         }
                         for _ in 0..20000 {
                             let l1 = tmp.load();
+                            black_box(l1);
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
+    c.bench_function("aarc_read_light_multi", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..20 {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            let l1 = tmp.load(Ordering::Acquire);
                             black_box(l1);
                         }
                     }));
@@ -1055,6 +1360,45 @@ fn main() {
         });
     });
 
+    c.bench_function("aarc_read_heavy_multi", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..20 {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            let l1 = tmp.load(Ordering::Acquire);
+                            let l2 = tmp.load(Ordering::Acquire);
+                            let l3 = tmp.load(Ordering::Acquire);
+                            let l4 = tmp.load(Ordering::Acquire);
+                            let l5 = tmp.load(Ordering::Acquire);
+                            black_box(l1);
+                            black_box(l2);
+                            black_box(l3);
+                            black_box(l4);
+                            black_box(l5);
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
     // ---
 
     c.bench_function("update_single", |b| {
@@ -1147,6 +1491,36 @@ fn main() {
         });
     });
 
+    c.bench_function("aarc_update_single", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..1 {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            tmp.store(Arc::new(random()), Ordering::Release);
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
     // ---
 
     c.bench_function("update_multi", |b| {
@@ -1225,6 +1599,36 @@ fn main() {
                         }
                         for _ in 0..20000 {
                             tmp.store(Arc::new(random()));
+                        }
+                    }));
+                }
+                let start = Instant::now();
+                started.store(true, Ordering::Release);
+                threads
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap());
+                diff += start.elapsed();
+            }
+            diff
+        });
+    });
+
+    c.bench_function("aarc_update_multi", |b| {
+        let tmp = Arc::new(AtomicArc::new(Some(AArc::new(0))));
+        b.iter_custom(|iters| {
+            let mut diff = Duration::default();
+            for _ in 0..iters {
+                let started = Arc::new(AtomicBool::new(false));
+                let mut threads = vec![];
+                for _ in 0..20 {
+                    let tmp = tmp.clone();
+                    let started = started.clone();
+                    threads.push(thread::spawn(move || {
+                        while !started.load(Ordering::Acquire) {
+                            spin_loop();
+                        }
+                        for _ in 0..20000 {
+                            tmp.store(Arc::new(random()), Ordering::Release);
                         }
                     }));
                 }
